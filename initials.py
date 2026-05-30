0d48afe6e20a1986e12268d9cc5a1707351eab18
Darlington Deladem Kegu <2425402725@live.gctu.edu.gh>
2026-05-30 08:08:37 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Darlington Deladem Kegu
+# STUDENT ID: 2425402725
+# Email: 2425402725@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Darlington Deladem Kegu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # prints my initials in stylized large letters 
+    print()
+    print('...DDK')
+    print()
+    
+    print('...DDDDDDDD............DDDDDDDD............KK........KK.....')
+    print('...DDDDDDDDDD..........DDDDDDDDDD..........KK......KK.......')
+    print('...DD.......DD.........DD.......DD.........KK....KK.........')
+    print('...DD........DD........DD........DD........KK..KK...........')
+    print('...DD........DD........DD........DD........KKKK.............')
+    print('...DD........DD........DD........DD........KKKK.............')
+    print('...DD........DD........DD........DD........KK..KK...........')
+    print('...DD.......DD.........DD.......DD.........KK....KK.........')
+    print('...DDDDDDDDDD.....**...DDDDDDDDDD.....**...KK......KK.....**')
+    print('...DDDDDDDD.......**...DDDDDDDD.......**...KK........KK...**')
+    print()
 
 
 main()
